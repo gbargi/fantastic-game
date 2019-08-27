ff1715fb064d2dc0435dd016d3b1f1e66a410e31
gbargi <54582806+gbargi@users.noreply.github.com>
2019-08-27 14:52:06 +0200

    Add files via upload

---
+++ Unit1.h
@@ -0,0 +1,31 @@
+//---------------------------------------------------------------------------
+
+#ifndef Unit1H
+#define Unit1H
+//---------------------------------------------------------------------------
+#include <Classes.hpp>
+#include <Controls.hpp>
+#include <StdCtrls.hpp>
+#include <Forms.hpp>
+#include <ExtCtrls.hpp>
+#include <Graphics.hpp>
+#include <jpeg.hpp>
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE-managed Components
+        TImage *Image1;
+        TImage *Image2;
+        TImage *Image3;
+        TImage *Image4;
+        TImage *Image5;
+        TImage *Image6;
+        TImage *Image7;
+private:	// User declarations
+public:		// User declarations
+        __fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
